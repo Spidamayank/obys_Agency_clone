3b27e6bdaaa7fe54da95cbc8c957c00d32ee0083
mayank <mayankrajawat3@gmail.com>
2024-09-27 21:41:23 +0530

    Add files via upload
    Obys Agency "page1-done successfully".............

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function loadingAnimation(){
+
+
 var tl = gsap.timeline()
 
 tl.from(".line h1",{
@@ -41,3 +44,24 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav",{
+    opacity:0
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+} 
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+     }) 
+     Shery.makeMagnet("#nav-part2 h4");
+}
+loadingAnimation();
+cursorAnimation()
+ 
